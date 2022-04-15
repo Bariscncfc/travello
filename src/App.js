6de7f71381a43e854ae232b2f7f2ac5c061c0416
Barış Can Çifçi <78167969+Bariscncfc@users.noreply.github.com>
2022-04-15 11:01:34 +0300

    Navbar
    navbar section is created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-     
+     <header>
+       <Navbar/>
+     </header>
     </div>
   );
 }
